c5cd1b70a3a2750f49708968d777c2f66673680b
cwm9cwm9 <github.reg@chiem.ma>
2015-09-11 09:48:17 -1000

    Changed to 32 bit timer, added const u64 tests

--- ISALatencyTest/test_template.h
+++ ISALatencyTest/test_template.h
@@ -6,30 +6,30 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// Change PTX_ISA_VERSION_2_0 to the PTX ISA Version number required BY THE INSTRUCTION.  Don't put your card's compute capability here...
-#ifdef PTX_ISA_VERSION_2_0
+#ifdef PTX_ISA_VERSION_2_0															   // Change PTX_ISA_VERSION_2_0 to the PTX ISA Version number
+                                                                                       // REQUIRED BY THE INSTRUCTION.  Don't put your card's compute capability here...
 
-// Replace INSTRUCTION with the instruction you are testing
-// Replace PARAMTYPE1, PARAMTYPE2 etc. with the type of parameters you are testing (const, u32, whatever)
-// Replace INPUT with the input data to the test instruction
-// The FUNCTION NAME will be PRINTED IN THE OUTPUT, so please be descriptive!
-__device__ unsigned int INSTRUCTION_PARAMTYPE1_PARAMTYPE2_INPUT(bool printresult)
+                                                                                       // Replace INSTRUCTION with the instruction you are testing
+                                                                                       // Replace PARAMTYPE1, PARAMTYPE2 etc. with the type of parameters you are testing (const, u32, whatever)
+                                                                                       // Replace INPUT with the input data to the test instruction
+                                                                                       // The FUNCTION NAME will be PRINTED IN THE OUTPUT, so please be descriptive!
+__device__ unsigned INSTRUCTION_PARAMTYPE1_PARAMTYPE2_INPUT(bool printresult)
 {
-	volatile unsigned long long start, stop;
+	volatile unsigned start, stop;
 
-	volatile unsigned out32a;										                // If your instruction generates output data from input data, you need a variable to capture it
+	volatile unsigned out32a;										                   // If your instruction generates output data from input data, you need a variable to capture it
 
 	asm("{                               \n\t"
-		" mov.u64 %0, %%clock64;         \n\t"
-		"                                \n\t"						                // If you need it, put setup code here
-		" bfind.u32  %2, 0x0U;           \n\t"						                // YOUR TEST INSTRUCTION GOES HERE.  THERE SHOULD ONLY BE ONE INSTRUCTION
-		"                                \n\t"						                // If you need it, put post code here
-		" mov.u64 %1, %%clock64;         \n\t"
+		" mov.u32 %0, %%clock;           \n\t"
+		"                                \n\t"						                   // If you need it, put setup code here
+		" bfind.u32  %2, 0x0U;           \n\t"						                   // YOUR TEST INSTRUCTION GOES HERE.  THERE SHOULD ONLY BE ONE INSTRUCTION
+		"                                \n\t"						                   // If you need it, put post code here
+		" mov.u32 %1, %%clock;           \n\t"
 		"}"
-		: "=l"(start), "=l"(stop), "=r"(out32a));					                // Different size registers need different letters.  l=64 bits, r=32 bits...
+		: "=r"(start), "=r"(stop), "=r"(out32a));					                   // Different size registers need different letters.  l=64 bits, r=32 bits...
 
-	if (printresult) printf("%s result = %u", __FUNCTION__, out32a);                // When printresult is true, print the name of the function along with any results
-	return stop - start - 106;
+	if (printresult) printf("%s result = %u", __FUNCTION__, out32a);                   // When printresult is true, print the name of the function along with any results
+	return stop - start - 42;
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -40,16 +40,16 @@ __device__ unsigned int INSTRUCTION_PARAMTYPE1_PARAMTYPE2_INPUT(bool printresult
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-__device__ void After_INSTRUCTION();											    // Replace INSTRUCTION with your instruction name
+__device__ void After_INSTRUCTION();											       // Replace INSTRUCTION with your instruction name
 
 __device__ void NEXT_TEST_GROUP()
 {
-	Evaluate(&INSTRUCTION_PARAMTYPE1_PARAMTYPE2_INPUT);								// call evaluate on the address of every function you've created
+	Evaluate(&INSTRUCTION_PARAMTYPE1_PARAMTYPE2_INPUT);								   // call evaluate on the address of every function you've created
 
-	After_INSTRUCTION();											                // Replace INSTRUCTION with your instruction name
+	After_INSTRUCTION();											                   // Replace INSTRUCTION with your instruction name
 }
 
 #undef NEXT_TEST_GROUP
-#define NEXT_TEST_GROUP After_INSTRUCTION											// Replace INSTRUCTION with your instruction name
+#define NEXT_TEST_GROUP After_INSTRUCTION											   // Replace INSTRUCTION with your instruction name
 
 #endif
